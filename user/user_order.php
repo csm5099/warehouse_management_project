3c43cef959cd269b55937ee613c6799db94ae93e
banjuhyeong <mrkii5280@gmail.com>
2022-12-03 14:53:15 +0900

    user_stock_add 수정

--- user/user_order.php
+++ user/user_order.php
@@ -22,14 +22,10 @@
 
   <?php
     #변수 선언
-    $product_no =  (int)$_POST["product_no"];       //일련번호
-    $product_grade =  (int)$_POST["product_grade"]; //평점 
+    $product_no =  (int)$_POST["product_no"];       //일련번호 
     $product_nm = $_POST["product_nm"];             //상품명
-    $product_sales = (int)$_POST["product_sales"];  //수량
     $product_price = (int)$_POST["product_price"];  //가격 
     $product_amt = (int)$_POST["product_amt"];        //재고량  
-    $product_state = $_POST["product_state"];       //상태 
-    $product_dt = $_POST["product_dt"];             //입고일
 
     #db 연결
     $database = "warehouse";
@@ -38,16 +34,14 @@
     mysql_select_db($database, $connect);
 
     #insert 쿼리
-    $query1 = "insert into product_tb values('$product_no','$product_grade','$product_nm','$product_sales','$product_price','$product_amt','$product_state','$product_dt')";   
-    $result1 = mysql_query($query1,$connect);   
+    $query1 = "insert into product_tb values('$product_no','$product_nm','$product_price','$product_amt');
     
     
 
 
     print "<center><font color=red size=5><b> 주문 결과입니다. </b></font></center>";
         print "<table border=1 align=center>";
-        print "<tr><td> 일련번호 </td><td> 평점 </td><td> 상품명 </td><td>수량 </td><td> 가격 </td>";
-        print "<td> 재고량 </td><td> 상태 </td><td> 입고일 </td></tr><br>";
+        print "<tr><td> 일련번호 </td><td> 상품명 </td><td> 가격 </td><td> 재고량 </td></tr><br>";
         
         
         $query = "select * from product_tb where product_no_pk = (select max(product_no_pk) from product_tb)";
