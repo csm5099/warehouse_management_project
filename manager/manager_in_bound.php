a8e7e74e417685541babf0020109133b5d757237
banjuhyeong <mrkii5280@gmail.com>
2022-12-03 17:56:15 +0900

    manager_in_bound, out_bound & out_bound_search 수정

--- manager/manager_in_bound.php
+++ manager/manager_in_bound.php
@@ -24,33 +24,37 @@
   <?php
   $product_no = $_POST['product_no'];
  
-  $database="warehouse";
+  $database="wearhouse";
   $connect= mysql_connect('localhost','lcw','chaewon') or die("mySQL 서버 연결 Error!");
 
   mysql_select_db($database, $connect);
-  $query= "SELECT * FROM product_tb";
-  
+
+  $query= "select * from delivery_tb, product_tb  where delivery_tb.product_no_pk  = product_tb.product_NO_PK;";
   $result = mysql_query($query, $connect);
 
-
-  print "<center><font color=black size=5><b>입고 조회 결과 </b></font></center>";
+  print "<center><font color=black size=5><b>입고현황</b></font></center>";
   print "<table border=1 align=center>";
   
-  print "<tr><td> 일련번호</td><td>평점</td><td> 상품명</td><td>입고량
-  </td><td>가격</td><td>재고량</td><td>상태</td><td>입고일</td></tr>";
-  $num= mysql_num_rows($result);
+  print "<tr><td> 인덱스</td><td> 송장번호</td><td> 일련번호</td>
+  <td> 배송일자</td><td> 도착일자</td><td> 배송수량</td>
+  <td> 사업자</td><td> 일련번호</td><td>상품명</td><td>판매량
+  </td><td>가격</td><td>재고량</td></tr>";
+
+  $num=mysql_num_rows($result);
  
+  
   for($i=0; $i<$num; $i++) {
-  $ans=mysql_fetch_row($result);
+    $ans=mysql_fetch_row($result);
+  
+    print "<tr><td>".$ans[0]."</td><td>".$ans[1]."</td><td>".$ans[2];
+    print "</td><td>".$ans[3]."</td><td>".$ans[4]."</td>";
+    print "<td>".$ans[5]."</td><td>".$ans[6]."</td><td>".$ans[7]."</td><td>".$ans[8]."</td>
+    <td>".$ans[9]."</td><td>".$ans[10]."</td><td>".$ans[11]."</td></tr><br>";
+    }
+  
+    print "</table>";
 
-  print "<tr><td>".$ans[0]."</td><td>".$ans[1]."</td><td>".$ans[2];
-  print "</td><td>".$ans[3]."</td><td>".$ans[4]."</td>";
-  print "<td>".$ans[5]."</td><td>".$ans[6]."</td><td>".$ans[7]."</td></tr><br>";
-  }
-
-  print "</table>";
-
-  mysql_close($connect);
+    mysql_close($connect);
 
   ?>
 
--- manager/manager_out_bound.php
+++ manager/manager_out_bound.php
@@ -28,27 +28,31 @@
        
       </select>
   <!-- 일련번호 입력-->
-  <input type="sumbit" name="product_no_pk" size="40" > <button>조회</button>  
+  <input type="sumbit" name="product_no_pk" size="40" > <button>조회</button> 
+ 
 </form>
 <!--type확인-->
 
   <!--<button>조회</button> -->
 
   <?php
-  $database="warehouse";
+  $database="wearhouse";
   $connect= mysql_connect('localhost','lcw','chaewon') or die("mySQL 서버 연결 Error!");
 
   mysql_select_db($database, $connect);
+
+
   //전체 데이터 가져오기
-  $query= "SELECT * FROM product_tb";
-  
+  $query= "select * from delivery_tb, product_tb  where delivery_tb.product_no_pk  = product_tb.product_NO_PK;";
   $result = mysql_query($query, $connect);
 
   print "<center><font color=black size=5><b>입고현황</b></font></center>";
   print "<table border=1 align=center>";
   
-  print "<tr><td> 일련번호</td><td>평점</td><td>수량</td><td>x
-  </td><td>가격</td><td>재고량</td><td>상태</td><td>입고일</td></tr>";
+  print "<tr><td> 인덱스</td><td> 송장번호</td><td> 일련번호</td>
+  <td> 배송일자</td><td> 도착일자</td><td> 배송수량</td>
+  <td> 사업자</td><td> 일련번호</td><td>상품명</td><td>판매량
+  </td><td>가격</td><td>재고량</td></tr>";
 
   $num=mysql_num_rows($result);
  
@@ -58,7 +62,8 @@
   
     print "<tr><td>".$ans[0]."</td><td>".$ans[1]."</td><td>".$ans[2];
     print "</td><td>".$ans[3]."</td><td>".$ans[4]."</td>";
-    print "<td>".$ans[5]."</td><td>".$ans[6]."</td><td>".$ans[7]."</td></tr><br>";
+    print "<td>".$ans[5]."</td><td>".$ans[6]."</td><td>".$ans[7]."</td><td>".$ans[8]."</td>
+    <td>".$ans[9]."</td><td>".$ans[10]."</td><td>".$ans[11]."</td></tr><br>";
     }
   
     print "</table>";
@@ -66,7 +71,8 @@
 mysql_close($connect);
 
 ?>
-
+<INPUT type="hidden"  id="delevery_adt" name="delevery_adt" value="<?php echo $delevery_adt?>" />
+<!--도착날짜manager_out_bound_search.php로 전송-->
 </main>
 <footer>footer</footer>
 </body>
