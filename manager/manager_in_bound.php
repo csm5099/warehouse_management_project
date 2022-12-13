11250773e8a49ab39605b51334b48a09bf1db363
choisungmin <76083571+csm5099@users.noreply.github.com>
2022-12-14 03:22:13 +0900

    입고 전체 출력

--- manager/manager_in_bound.php
+++ manager/manager_in_bound.php
@@ -25,45 +25,51 @@
   </nav>
   <main>
     <?php
-	include '../Check_Cookie_manager.php';
-?>
+	    include '../Check_Cookie_manager.php';
+    ?>
+
     <h1>관리자</h1>
     <h2>입고관리</h2>
+
     <?php
-  $product_no = $_POST['product_no'];
- 
-  $database="warehouse";
-  $connect= mysql_connect('localhost','root','root') or die("mySQL 서버 연결 Error!");
+    $database="warehouse";
+    $connect= mysql_connect('localhost','root','root') or die("mySQL 서버 연결 Error!");
+    mysql_select_db($database, $connect);
+    $query= "SELECT * FROM product_tb";
 
-  mysql_select_db($database, $connect);
-  $query= "SELECT * FROM product_tb";
-  
-  $result = mysql_query($query, $connect);
+    $query= 
+    "SELECT *
+    FROM product_tb P, delivery_tb D
+    WHERE P.product_no_pk = D.product_no_pk
+    ";
 
+    $result = mysql_query($query, $connect);
 
-  print "<center><font color=black size=5><b>입고 조회 결과 </b></font></center>";
-  print "<table border=1 align=center>";
-  
-  print "<tr><td> 일련번호</td><td>평점</td><td> 상품명</td><td>입고량
-  </td><td>가격</td><td>재고량</td><td>상태</td><td>입고일</td></tr>";
-  $num= mysql_num_rows($result);
- 
-  for($i=0; $i<$num; $i++) {
-  $ans=mysql_fetch_row($result);
+    print "<center><h1>입고 조회 결과</h1></center>";
+    print "<table border=1 align=center>";
+    print
+    "<tr>
+    <td> 외주 업체</td>
+    <td> 상품 일련 번호</td>
+    <td> 상품명</td>
+    <td> 현 재고량</td>
+    </tr>";
 
-  print "<tr><td>".$ans[0]."</td><td>".$ans[1]."</td><td>".$ans[2];
-  print "</td><td>".$ans[3]."</td><td>".$ans[4]."</td>";
-  print "<td>".$ans[5]."</td><td>".$ans[6]."</td><td>".$ans[7]."</td></tr><br>";
-  }
-
-  print "</table>";
-
-  mysql_close($connect);
-
+    $num= mysql_num_rows($result);
+    for($i=0; $i<$num; $i++) {
+      $ans=mysql_fetch_row($result);
+      print
+      "<tr>
+      <td>".$ans[0]."</td>
+      <td>".$ans[1]."</td>
+      <td>".$ans[2]."</td>
+      <td>".$ans[5]."</td>
+      </tr>
+      ";
+    }
+    print "</table>";
+    mysql_close($connect);
   ?>
-
-
-
   </main>
   <footer>
 
